9ed33a6fadbd924f227035d2201d22d739a499fc
Arjan Hoek <arjanhoek5191@gmail.com>
2022-01-25 08:25:01 +0100

    add listener to the check button

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const checkBtn = document.querySelector('.check');
+checkBtn.addEventListener('click', function () {
+  const messageEl = document.querySelector('.message');
+
+  const inputEl = document.querySelector('.guess');
+  const inputValue = Number(inputEl.value);
+  console.log(inputValue);
+
+  if (!inputValue) {
+    messageEl.textContent = 'No number!';
+  } else {
+    messageEl.textContent = 'Try again!';
+  }
+});
